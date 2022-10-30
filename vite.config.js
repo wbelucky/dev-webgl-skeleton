8d0006c0190689b8eba45dc265efacc781427f58
wbelucky <39439193+WBelucky@users.noreply.github.com>
2022-10-30 17:24:47 +0900

    disable minify option for submit

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,14 @@
-import { defineConfig } from 'vite'
-import { viteSingleFile } from 'vite-plugin-singlefile'
+import { defineConfig } from 'vite';
+import { viteSingleFile } from 'vite-plugin-singlefile';
 import glsl from 'vite-plugin-glsl';
 
 export default defineConfig({
   root: 'src',
-  plugins: [
-        viteSingleFile(),
-        glsl()
-  ],
+  plugins: [viteSingleFile(), glsl()],
   build: {
     cssCodeSplit: false,
+    minify: false,
     assetsInlineLimit: 100000000,
-    outDir: '../dist'
-  }
-})
+    outDir: '../dist',
+  },
+});
